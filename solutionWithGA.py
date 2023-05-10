3aecb468cb7fbef3a23ac9dbfd216f773f8e5d12
eva-pomposo <evabartolomeu@ua.pt>
2023-05-10 19:15:05 +0100

    comment

--- solutionWithGA.py
+++ solutionWithGA.py
@@ -16,7 +16,7 @@ NUM_EDGES = 1225
 
 adj_matrix = np.zeros((NUM_VERTICES, NUM_VERTICES))
 
-# randomly assign 290 edges in the graph
+# randomly assign NUM_EDGES edges in the graph
 for i in range(NUM_EDGES):
     # randomly select two vertices
     vertex1 = np.random.randint(0, NUM_VERTICES)
@@ -31,7 +31,6 @@ for i in range(NUM_EDGES):
 # Number of colors available
 num_colors = NUM_VERTICES
 
-
 def fitness_func(solution, solution_idx):
     """Calculate the fitness of the solution."""
     conflicts = 0
@@ -44,23 +43,18 @@ def fitness_func(solution, solution_idx):
 
 
 def on_generation(ga_instance):
-    """Check if the population has converged and
-    Print the fitness of the best solution in each generation
-    """
-    print(
-        f'Generation {ga_instance.generations_completed}: Best fitness = {ga_instance.best_solution()}')
+    """Check if the population has converged and print the fitness of the best solution in each generation"""
+    print(f'Generation {ga_instance.generations_completed}: Best fitness = {ga_instance.best_solution()}')
 
     n = 10  # number of generations to check
     best_fitness = ga_instance.best_solution()[1]
     last_n_fitness = [s[1] for s in ga_instance.best_solutions[-n:]]
     print(last_n_fitness)
     if all(abs(f - best_fitness) < 0.0001 for f in last_n_fitness):
-        print(
-            f"Population has converged. Best fitness value has not changed significantly over the last {n} generations.")
+        print(f"Population has converged. Best fitness value has not changed significantly over the last {n} generations.")
         return "stop"
 
     return "stop" if ga_instance.best_solution()[1] == 1 else False
-
 
 def run_ga():
     """Run the genetic algorithm to find a solution."""
@@ -68,8 +62,7 @@ def run_ga():
                            num_parents_mating=50,  # Number of solutions to be selected as parents
                            fitness_func=fitness_func,
                            sol_per_pop=100,  # population size
-                           # : Number of genes in the solution/chromosome
-                           num_genes=len(adj_matrix),
+                           num_genes=len(adj_matrix), # Number of genes in the solution/chromosome
                            gene_type=int,
                            gene_space=list(range(num_colors)),
                            on_generation=on_generation,
@@ -78,8 +71,7 @@ def run_ga():
     ga_instance.run()
 
     # Print the best solution
-    print('Best solution:', ga_instance.best_solution()[
-          0], " ,score", ga_instance.best_solution()[1])
+    print('Best solution:', ga_instance.best_solution()[0], ", score", ga_instance.best_solution()[1])
 
 
 if __name__ == '__main__':
