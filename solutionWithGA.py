8050a43bcd07eaa40f732d0b8b3d184a02ccff36
bernas04 <joaobernardo0@ua.pt>
2023-05-11 14:31:04 +0100

    ok eva

--- solutionWithGA.py
+++ solutionWithGA.py
@@ -14,6 +14,16 @@ import numpy as np
 NUM_VERTICES = 50
 NUM_EDGES = 1225
 
+# PyGAD parameters
+NUM_GENERATIONS = 2000
+NUM_PARENTS_MATING = 50
+NUM_SOLUTIONS_PER_POPULATION = 100
+
+
+saturation = str(int(NUM_GENERATIONS * 0.1))  # 20% of the generations
+STOP_CRITERIA = ["reach_1", "saturate_" + saturation]
+
+
 adj_matrix = np.zeros((NUM_VERTICES, NUM_VERTICES))
 
 # randomly assign NUM_EDGES edges in the graph
@@ -31,11 +41,12 @@ for i in range(NUM_EDGES):
 # Number of colors available
 num_colors = NUM_VERTICES
 
+
 def fitness_func(solution, solution_idx):
     """Calculate the fitness of the solution."""
     conflicts = 0
     for i in range(len(adj_matrix)):
-        for j in range(i+1, len(adj_matrix)):
+        for j in range(i + 1, len(adj_matrix)):
             if adj_matrix[i][j] and solution[i] == solution[j]:
                 conflicts += 1
     # return a fitness that minimizes the number of conflicts
@@ -44,35 +55,36 @@ def fitness_func(solution, solution_idx):
 
 def on_generation(ga_instance):
     """Check if the population has converged and print the fitness of the best solution in each generation"""
-    print(f'Generation {ga_instance.generations_completed}: Best fitness = {ga_instance.best_solution()}')
+    print(
+        f"Generation {ga_instance.generations_completed}: Best fitness = {ga_instance.best_solution()}"
+    )
 
-    n = 10  # number of generations to check
-    best_fitness = ga_instance.best_solution()[1]
-    last_n_fitness = [s[1] for s in ga_instance.best_solutions[-n:]]
-    print(last_n_fitness)
-    if all(abs(f - best_fitness) < 0.0001 for f in last_n_fitness):
-        print(f"Population has converged. Best fitness value has not changed significantly over the last {n} generations.")
-        return "stop"
-
-    return "stop" if ga_instance.best_solution()[1] == 1 else False
 
 def run_ga():
     """Run the genetic algorithm to find a solution."""
-    ga_instance = pygad.GA(num_generations=2000,
-                           num_parents_mating=50,  # Number of solutions to be selected as parents
-                           fitness_func=fitness_func,
-                           sol_per_pop=100,  # population size
-                           num_genes=len(adj_matrix), # Number of genes in the solution/chromosome
-                           gene_type=int,
-                           gene_space=list(range(num_colors)),
-                           on_generation=on_generation,
-                           save_best_solutions=True)
+    ga_instance = pygad.GA(
+        num_generations=NUM_GENERATIONS,
+        num_parents_mating=NUM_PARENTS_MATING,  # Number of solutions to be selected as parents
+        fitness_func=fitness_func,
+        sol_per_pop=NUM_SOLUTIONS_PER_POPULATION,  # population size
+        # Number of genes in the solution/chromosome
+        num_genes=len(adj_matrix),
+        gene_type=int,
+        gene_space=list(range(num_colors)),
+        on_generation=on_generation,
+        stop_criteria=STOP_CRITERIA,
+    )
 
     ga_instance.run()
 
     # Print the best solution
-    print('Best solution:', ga_instance.best_solution()[0], ", score", ga_instance.best_solution()[1])
+    print(
+        "Best solution:",
+        ga_instance.best_solution()[0],
+        ", score",
+        ga_instance.best_solution()[1],
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_ga()
